f46fba627409156fc93e5d45d8e736eb5647ff79
Vinh <vinh.bui0101@gmail.com>
2022-02-09 23:33:46 -0800

    Finished 5th

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
 const sumAll = function(first, last) {
-  var sum;
-  for (let x = first; x <= last; first++){
+  let sum = 0;
+  if(first > last){
+    let temp = 0;
+    temp = first;
+    first = last;
+    last = temp;
+  }else if(Math.sign(first,last) == -1){
+    return "ERROR"
+  }else if(typeof(first) != typeof(last)){
+    return "ERROR"
+  }
+  for (first; first <= last; first++){
     sum += first;
   }
   return sum;
