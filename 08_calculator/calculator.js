3a6c4b66cffbd2617050f00289580b15e5a27a2a
Vinh <vinh.bui0101@gmail.com>
2022-02-15 15:51:49 -0800

    cleaner code

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,14 @@
-const add = function() {
-	let answer = 0
-  for(let x = 0; x < arguments.length; x++){
-    answer += arguments[x];
-  }
-  //[Arguments] { '0': 2, '1': 6 }
-  return answer;
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-  let answer = arguments[0] - arguments[1]
-  return answer;
+const subtract = function(a,b) {
+  return a - b;
 };
 
-const sum = function() {
+const sum = function(array) {
   //[Arguments] { '0': [] }
   let answer = 0;
-  const array = arguments[0];
   if(array.length == 0){
     return 0;
   }else{
@@ -26,10 +19,9 @@ const sum = function() {
   }
 };
 
-const multiply = function() {
+const multiply = function(array) {
   //[Arguments] { '0': [] }
   let answer = 1;
-  const array = arguments[0];
   if(array.length == 0){
     return 0;
   }else{
@@ -45,12 +37,12 @@ const power = function() {
   return answer;
 };
 
-const factorial = function() {
+const factorial = function(n) {
   let answer = 1;
-  if(arguments[0] == 0){
+  if(n == 0){
     return 1;
   }else{
-    for(let i = arguments[0]; i >= 1;i--){
+    for(let i = n; i >= 1;i--){
       answer = answer * i;
     }
     return answer;
