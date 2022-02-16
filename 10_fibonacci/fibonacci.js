eebbe45089362b8407e0b90246d119d6cc5d46e6
Vinh <vinh.bui0101@gmail.com>
2022-02-15 22:01:49 -0800

    Finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  sum = 0
+  num1 = 0 
+  num2 = 1
+  if(num == 1 ) return 1;
+  else if(num < 0) return "OOPS";
+  else{
+    for(let i = 1; i < num; i++){
+      sum = num1 + num2;
+      num1 = num2;
+      num2 = sum;
+    }
+    return num2;
+  }
 };
 
 // Do not edit below this line
