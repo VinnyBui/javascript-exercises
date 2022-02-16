7f15f1127a6d24c13616b9bb2902666398e66d58
Vinh <vinh.bui0101@gmail.com>
2022-02-15 23:01:29 -0800

    Finish FindtheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(person) {
+  let oldest = 0;
+  let position;
+  objLen = Object.keys(person).length;
+  for(let x = 0; x < objLen; x++){
+    if(oldest < (person[x].yearOfDeath - person[x].yearOfBirth)){
+      oldest = person[x].yearOfDeath - person[x].yearOfBirth
+      position = x
+    }
+  }
+  return person[position];
 };
 
 // Do not edit below this line
