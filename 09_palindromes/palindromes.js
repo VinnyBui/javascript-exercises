6e13ff0cc2a7e149c8fc53715b79d8b70303333b
Vinh <vinh.bui0101@gmail.com>
2022-02-15 21:46:59 -0800

    Finished plaindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.split(" ").join("").replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+  let len = str.length
+  for (var i = 0; i < len/2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+        return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
