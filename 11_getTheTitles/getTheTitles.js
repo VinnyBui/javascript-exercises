206f56f35aaa5619299a053e29a3a3954edc5ca2
Vinh <vinh.bui0101@gmail.com>
2022-02-15 22:21:14 -0800

    Finished getTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(book) {
+  let objLen = Object.keys(book).length;
+  let answer = []
+  for(let x = 0; x < objLen; x++){
+    answer.push(book[x].title);
+  }
+  return answer
 };
 
 // Do not edit below this line
